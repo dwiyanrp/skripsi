2b463d19f82d38c4501dfb02967d7850bf3dd0f9
Dwiyan Rezkia Putra <shadow.drp@gmail.com>
2019-08-22 00:08:52 +0700

    cp

---
+++ script/app.js
@@ -0,0 +1,32 @@
+const Web3 =  require('web3')
+const fs = require('fs');
+
+const config = {
+    url: "http://178.128.98.34:8545",
+    abi:  [ { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_deviceType", "type": "uint8" } ], "name": "addDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" }, { "name": "_startTime", "type": "uint32" }, { "name": "_endTime", "type": "uint32" } ], "name": "addRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "deleteDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" } ], "name": "deleteRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_name", "type": "bytes20" } ], "name": "setManager", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "name", "type": "bytes20" } ], "name": "ManagerEvent", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "typeEvent", "type": "int8" }, { "indexed": false, "name": "deviceID", "type": "bytes20" } ], "name": "DeviceEvent", "type": "event" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "getDevice", "outputs": [ { "name": "", "type": "bytes17" }, { "name": "", "type": "uint8" }, { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getManager", "outputs": [ { "name": "", "type": "address" }, { "name": "", "type": "bytes20" }, { "name": "", "type": "bytes17[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "getRule", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "view", "type": "function" } ],
+    address: "0x7ce63106ddedbd5e430e602052ac02e02c76c3b7"
+}
+
+let rawdata = fs.readFileSync('data.json');
+let data = JSON.parse(rawdata);
+
+const web3 = new Web3(config.url);
+const contract = new web3.eth.Contract(config.abi, config.address);
+var address,result;
+main();
+
+async function main() {
+    result = await web3.eth.getAccounts();
+    address = result[0];
+    const startTime = new Date();
+
+    data.forEach(macaddr => {
+        let byte = "0x";
+        for (let i = 0; i < macaddr.length; i++) {
+            byte += macaddr.charCodeAt(i)
+        }
+        console.log(byte.length)
+    });
+
+    console.info('Execution time: %dms', (new Date() - startTime))
+}
