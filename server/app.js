411cfe17c9f7e28dff85da596fc3f98545daee6c
Dwiyan Rezkia Putra <dwiyan.putra@tokopedia.com>
2019-08-30 19:24:25 +0700

    cp

--- server/app.js
+++ server/app.js
@@ -1,24 +1,57 @@
 const Web3 =  require('web3')
 const fastify = require('fastify')()
-const utf8 = require('utf8');
 // const fastify = require('fastify')({ logger: { level: 'info'} })
 
 const config = {
     url: "http://139.59.104.37:8545",
-    abi:  [ { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "deleteDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" } ], "name": "deleteRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "getDevice", "outputs": [ { "name": "", "type": "bytes17" }, { "name": "", "type": "uint8" }, { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" }, { "name": "_startTime", "type": "uint32" }, { "name": "_endTime", "type": "uint32" } ], "name": "addRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "getRule", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getManager", "outputs": [ { "name": "", "type": "address" }, { "name": "", "type": "bytes20" }, { "name": "", "type": "bytes17[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_name", "type": "bytes20" } ], "name": "setManager", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_deviceType", "type": "uint8" } ], "name": "addDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "name", "type": "bytes20" } ], "name": "ManagerEvent", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "typeEvent", "type": "int8" }, { "indexed": false, "name": "deviceID", "type": "bytes20" } ], "name": "DeviceEvent", "type": "event" } ],
-    address: "0x9f02b5b5ee1e321ac6aebb55d9bb7f9b8bf5af22"
+    abi: [ { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_deviceType", "type": "uint8" } ], "name": "addDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" }, { "name": "_startTime", "type": "uint32" }, { "name": "_endTime", "type": "uint32" } ], "name": "addRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "deleteDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" } ], "name": "deleteRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_name", "type": "bytes32" } ], "name": "setManager", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "name", "type": "bytes32" } ], "name": "ManagerEvent", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "typeEvent", "type": "int8" }, { "indexed": false, "name": "deviceID", "type": "bytes17" } ], "name": "DeviceEvent", "type": "event" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "getDevice", "outputs": [ { "name": "", "type": "bytes17" }, { "name": "", "type": "uint8" }, { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getManager", "outputs": [ { "name": "", "type": "address" }, { "name": "", "type": "bytes32" }, { "name": "", "type": "bytes17[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "getRule", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "view", "type": "function" } ],
+    address: "0xd637e7fee7df845ace71a552d734d3e3681c63a1"
 }
 
 const web3 = new Web3(config.url);
 const contract = new web3.eth.Contract(config.abi, config.address);
 var accounts;
 
+fastify.get('/mymanager', async (request, reply) => {
+    let userAddress = accounts[request.headers.authorization];
+    let result, error;
+    await contract.methods.getManager().call((err, res) => { error = err, result = res })
+
+    let resp = {
+        manager : {
+            address: result["0"],
+            name: web3.utils.hexToUtf8(result["1"])
+        }
+    }
+
+    return { data : resp, error: error }
+})
+
+fastify.post('/manager', async (request, reply) => {
+    let userAddress = accounts[request.headers.authorization];
+    let name = request.body.name;
+    let hexName = web3.utils.utf8ToHex(name);
+
+    let result;
+    await contract.methods.setManager(hexName).send({from: userAddress, gas: 3000000}).then((res) => {result = res})
+
+    return {result: result}
+})
+
 // Manager can add new device
 fastify.post('/device', async (request, reply) => {
     let userAddress = accounts[request.headers.authorization];
     let deviceID = request.body.device_id;
     let macAddress = web3.utils.utf8ToHex(deviceID);
-    return { user_address: userAddress, macAddress: macAddress}
+
+    let result, error;
+    await contract.methods.Hello(macAddress).call({from: userAddress}, (err, res) => {
+        console.log(res)
+        result = res;
+        error = err;
+    })
+
+    return { result : result, error : error, macAddress : macAddress }
 })
 
 // Manager can remove device
