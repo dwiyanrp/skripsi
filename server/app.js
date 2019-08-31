ed7ca5dccaf4e8d146edb0e8f25f838234720ca4
Dwiyan Rezkia Putra <dwiyan.putra@tokopedia.com>
2019-08-31 09:43:17 +0700

    cp

--- server/app.js
+++ server/app.js
@@ -5,13 +5,14 @@ const fastify = require('fastify')()
 const config = {
     url: "http://139.59.104.37:8545",
     abi: [ { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_deviceType", "type": "uint8" } ], "name": "addDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" } ], "name": "addRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "deleteDevice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_deviceID", "type": "bytes17" }, { "name": "_managerAddr", "type": "address" } ], "name": "deleteRule", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_name", "type": "bytes32" } ], "name": "editManager", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "name", "type": "bytes32" } ], "name": "ManagerEvent", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "typeEvent", "type": "int8" }, { "indexed": false, "name": "deviceID", "type": "bytes17" } ], "name": "DeviceEvent", "type": "event" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "checkAccess", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_deviceID", "type": "bytes17" } ], "name": "getDevice", "outputs": [ { "name": "", "type": "bytes17" }, { "name": "", "type": "uint8" }, { "name": "", "type": "address" }, { "name": "", "type": "bool" }, { "name": "", "type": "address[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getManager", "outputs": [ { "name": "", "type": "address" }, { "name": "", "type": "bytes32" }, { "name": "", "type": "bytes17[]" } ], "payable": false, "stateMutability": "view", "type": "function" } ],
-    address: "0x4a8ac95539789e3df12b79fd87e55c46c7cee662"
+    address: "0x381e81831131d3066ddbea36a4fe5e7b97cbc557"
 }
 
 const web3 = new Web3(config.url);
 const contract = new web3.eth.Contract(config.abi, config.address);
 var accounts;
 
+// Edit manager
 fastify.put('/manager', async (request, reply) => {
     let userAddress = request.headers.authorization;
     if (userAddress == "") {
@@ -22,12 +23,8 @@ fastify.put('/manager', async (request, reply) => {
     let hexName = web3.utils.utf8ToHex(name);
 
     let response,responseError;
-    await contract.methods.editManager(hexName).send({from: userAddress, gas: 3000000})
-    .on('receipt', function(receipt) {
-        response = receipt;
-    })
-    .on('error', function(err) {
-        console.log(err)
+    await contract.methods.editManager(hexName).send({from: userAddress, gas: 3000000}).then((result) => {
+        response = result;
     })
 
     return {data: response}
@@ -88,6 +85,7 @@ fastify.delete('/device', async (request, reply) => {
         return {error: "Device not exists"}
     }
 
+    let response,responseError;
     await contract.methods.deleteDevice(macAddress).send({from: userAddress, gas: 3000000})
     .on('receipt', function(receipt) {
         response = receipt;
@@ -103,6 +101,7 @@ fastify.delete('/device', async (request, reply) => {
 fastify.get('/device', async (request, reply) => {
     let macAddress = web3.utils.utf8ToHex(request.query.device_id);
 
+    let response,responseError;
     await contract.methods.getDevice(macAddress).call().then((result) => {
         if (!result["3"]) { // check is device exists
             responseError = "Device not exists"
@@ -112,11 +111,15 @@ fastify.get('/device', async (request, reply) => {
                     device_id: web3.utils.hexToUtf8(result["0"]),
                     device_type: result["1"],
                     owner_address: result["2"],
-                    device_rules: result["4"]
+                    // device_rules: result["4"]
                 }
             }
         }
     })
+
+    if (responseError) {
+        reply.code(400).send({error: responseError})
+    }
 
     return {data: response}
 })
@@ -179,6 +182,7 @@ fastify.get('/rule', async (request, reply) => {
         reply.code(400).send({error: "Unauthorized Account"})
     }
 
+    let response,responseError;
     let macAddress = web3.utils.utf8ToHex(request.query.device_id);
     await contract.methods.checkAccess(macAddress).call({from: userAddress}).then((result) => {
         response = {
